7fcc39134dfe3bdf6f20c6fec3eedde5a962b83e
marceloaviana <96154514+marceloaviana@users.noreply.github.com>
2023-11-08 16:08:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# Exercicio 15
+soma = 0
+
+for numero in range(0, 100+1):
+  soma += numero
+  
+print(f"A soma dos números de 0 a 100 é: {soma} - {numero}")
+
+
+print("----------------------------------------")
+# Exercicio 16
+
+
+numero = int(input("Digite um número: "))
+
+for i in range(0, numero+1):
+  print(i)
+
+
+print("----------------------------------------")
+# Exercicio 17
+
+
+for numero in range(0, 11, 2):
+  if numero % 2 == 0:
+    print(numero)
+
+
+print("----------------------------------------")
+# Exercicio 18
+
+
+for numero in range(1, 11, 2):
+  #if numero % 2 != 0:
+    print(numero)
